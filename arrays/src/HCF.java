98122008b4bf2783c78a8366421e9a9f698c7f6b
Ashita <ashitam2407@gmail.com>
2024-01-27 15:30:59 +0530

    add

--- arrays/src/HCF.java
+++ arrays/src/HCF.java
@@ -1,23 +1,27 @@
 public class HCF {
     public static void main(String[] args) {
-        int ans=calcGCD(72,72);
-        System.out.println(ans);
-    }
-    public static int calcGCD(int n, int m){
-        // Write your code here.
-        int d=0,q=0;
-        if(n>=m)
-        {  d=n;
-            q=m;}
-        else {
-            d=m;
-            q=n;}
-        int rem=0;
-        while(d%q!=0){
-            rem=d%q;
-            d=q;
-           q=rem;
-        }
-        return q;
+//        int ans=calcGCD(72,72);
+//        System.out.println(ans);
+//    }
+//    public static int calcGCD(int n, int m){
+//        // Write your code here.
+//        int d=0,q=0;
+//        if(n>=m)
+//        {  d=n;
+//            q=m;}
+//        else {
+//            d=m;
+//            q=n;}
+//        int rem=0;
+//        while(d%q!=0){
+//            rem=d%q;
+//            d=q;
+//           q=rem;
+//        }
+//        return q;
+        String s="tool";
+        int x=Integer.valueOf(s);
+        System.out.println(x);
     }
 }
+
