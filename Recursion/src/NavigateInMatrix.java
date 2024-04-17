abaf4768201f3e9fa15b72ef84389d93e460e432
Ashita <ashitam2407@gmail.com>
2024-04-17 18:36:48 +0530

    Navigating in amatrix

---
+++ Recursion/src/NavigateInMatrix.java
@@ -0,0 +1,19 @@
+public class NavigateInMatrix {
+    public static void main(String[] args) {
+        int row = 3;
+        int column=3;
+        Destination(0,0,"",row,column);
+    }
+
+    public static void Destination(int col,int row,String ans , int r,int c) {
+        if(row==r-1 && col==c-1){
+            System.out.println(ans);
+            return ;
+        }
+        if(row>r-1 || col>c-1){
+            return;
+        }
+        Destination(col+1,row,ans+"H",r,c);
+        Destination(col,row+1,ans+"V",r,c) ;
+    }
+}
