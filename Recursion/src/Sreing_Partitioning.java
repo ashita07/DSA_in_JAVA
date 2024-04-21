e7414d26f5922ff420b8e928e9b77d388b16e675
Ashita <ashitam2407@gmail.com>
2024-04-22 00:35:12 +0530

    Leetcode 131 String partitioning pallindrome

--- Recursion/src/Sreing_Partitioning.java
+++ Recursion/src/Sreing_Partitioning.java
@@ -1,22 +1,40 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Sreing_Partitioning {
     public static void main(String[] args) {
-        String ques="ashita";
+        String ques="aababb";
+        List<List<String>> ll1=new ArrayList<>();
         List<String> ll=new ArrayList<>();
-        Partition(ques,"",ll);
+        Partition(ques,"",ll,ll1);
+        System.out.println(ll1);
     }
-    public static void Partition(String ques,String ans,List<String> ll){
+    public static void Partition(String ques,String ans,List<String> ll,List<List<String>> ll1){
         if(ques.isEmpty()){
-            System.out.println(ll);
+            ll1.add(new ArrayList<>(ll));
             return;
         }
         for (int i=1;i<=ques.length();i++){
             String s=ques.substring(0,i);
-            ll.add(s);
-            Partition(ques.substring(i),ans+s+"|",ll);
-            ll.remove(s);
+            if(is_Pallindrome(s)){
+                ll.add(s);
+                Partition(ques.substring(i),ans+s+"|",ll,ll1);
+                ll.remove(ll.size()-1);
+            }
+
         }
     }
+  public static boolean is_Pallindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i) !=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+  }
 }
