a6507febcf1dfd4266b0dfec9be089ef18daf5ad
Ashita <ashitam2407@gmail.com>
2024-05-03 02:27:58 +0530

    greatest common divisor of string

--- arrays/src/GratestCommonDivisor_1071.java
+++ arrays/src/GratestCommonDivisor_1071.java
@@ -1,20 +1,15 @@
 public class GratestCommonDivisor_1071 {
     public static void main(String[] args) {
-        System.out.println(CommonD("ababab","abab"));
+        System.out.println(CommonD("abababababab","ababab"));
     }
     public static String CommonD(String str1,String str2){
-        String ans="";int smallLen=0;
-        if(str1.length()>str2.length()){
-            smallLen=str2.length();
-        }
-        else smallLen=str1.length();
+       if(str1.length()==str2.length()){
+           return str1;
+       }
+       if(str1.length()<str2.length()){
+           return CommonD(str2,str1);
+       }
+        return CommonD(str1.substring(str2.length()), str2);
 
-        for (int i = 0; i < smallLen; i++) {
-            if(str1.charAt(i)==str2.charAt(i)){
-                ans=ans+str1.charAt(i);
-            }
-            else break;
-        }
-       return ans;
     }
 }
